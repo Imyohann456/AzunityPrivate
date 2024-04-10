d93f52e7a33295d139407b65c4bb85e24b9365ed
ClumsyGuy <145586346+Imyohann456@users.noreply.github.com>
2024-04-11 07:04:13 +0800

    Add files via upload
    Signed-off-by: ClumsyGuy <145586346+Imyohann456@users.noreply.github.com>

---
+++ Modules/NewMainScript.lua
@@ -0,0 +1,2 @@
+--This watermark is used to delete the file if its cached, remove it to make the file persist after commits.
+The current thread cannot call 'HttpGetAsync' (lacking capability RobloxScript)
